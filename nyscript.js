cf711d8d914f15c7fd3316824128ed6eafe4882f
Andreas Palm <perandreaspalm@gmail.com>
2020-01-16 11:37:14 +0100

    Symbols added

--- nyscript.js
+++ nyscript.js
@@ -1,20 +1,35 @@
 const Suits = ["Spades", "Hearts", "Diamonds", "Clubs"];
+const SuitsSymbols = ["♠", "♥", "♦", "♣"];
 const Values = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
-var PlayerHand = [], DealerHand = [];
+var PlayerHand = [0], DealerHand = [0];
 
-/* function cardEval(aCard) {
-    let cardEval = 0;
-    if (aCard < 1) {
-        cardEval = 11;
+function cardEval(aCard) {
+    switch (aCard) {
+        case 'Ace':
+            return 11;
+        case 'King':
+            return 10;
+        case 'Queen':
+            return 10;
+        case 'Jack':
+            return 10;
+        default:
+            return aCard;
     }
-    else if (aCard >= 10) {
-        cardEval = 10;
+}
+
+function symbolizer(Suit) {
+    switch (Suit) {
+        case 'Spades':
+            return SuitsSymbols[0];
+        case 'Hearts':
+            return SuitsSymbols[1];
+        case 'Diamonds':
+            return SuitsSymbols[2];
+        case 'Clubs':
+            return SuitsSymbols[3];
     }
-    else {
-        cardEval = aCard + 1;
-    }
-    return cardEval;
-} */
+}
 
 // Function let's you shuffle arrays
 
@@ -68,17 +83,40 @@ function spawnDecks() {
     $('#h1-jumbo').addClass('d-none');
     $('#p-jumbo').addClass('d-none');
     $('#hr-jumbo').addClass('d-none');
-    newCard('#dealersHand',DealerDeck.deck.shift());
+    console.log('Dealer hand size ' + DealerHand.length);
+    DealerHand.unshift(DealerDeck.deck.shift());
+    DealerHand[DealerHand.length-1] += cardEval(DealerHand[0].value);
+    newCard('#dealersHand',DealerHand[0]);
+    console.log('Dealer hand size ' + DealerHand.length);
+    console.log(DealerHand[DealerHand.length-1])
     // console.log(deckShuffler(DealerDeck.deck));
 }
 
 // Card graphics
 
+function drawSymbols(values, suits) {
+    let symbols = symbolizer(suits);
+    for ( ; values > 1 ; values--) {
+        symbols += symbolizer(suits);
+    }
+    return symbols
+}
+
+function colorizer(suit) {
+    switch (suit) {
+        case 'Hearts':
+        case 'Diamonds':
+            return 'danger';
+        default:
+            return 'dark';
+    }
+}
+
 function newCard(drawer, title) {
     var cardwhole = $(`
     <div class="col">
         <div class="card drawn-cards" style="width: 10rem">
-            <h4 class="card-title">${title.value} of ${title.suit}</h4>
+            <h4 class="card-title">${title.value} <span class="text-${colorizer(title.suit)}">${drawSymbols(cardEval(title.value),title.suit)}</span></h4>
             <div class="card-body"></div>
         </div>
     </div>`
@@ -89,10 +127,54 @@ function newCard(drawer, title) {
 // Button adds card to hand
 
 $('#draw-button').click(function () {
-    newCard('#playersHand',DealerDeck.deck.shift());
+    PlayerHand.unshift(DealerDeck.deck.shift());
+    PlayerHand[PlayerHand.length-1] += cardEval(PlayerHand[0].value);
+    newCard('#playersHand',PlayerHand[0]);
+    console.log(PlayerHand);
+    console.log(cardEval(PlayerHand[PlayerHand.length-1]));
+    bustCheck('player',PlayerHand[PlayerHand.length-1]);
 });
 
-function handStopper(sum) {
+;
+
+$('#stand-button').click(function () {
+    DealerHand.unshift(DealerDeck.deck.shift());
+    DealerHand[DealerHand.length-1] += cardEval(DealerHand[0].value);
+    bustCheck('dealer',DealerHand[DealerHand.length-1]);
+});
+
+
+function bustCheck(drawer,sum) {
+    switch (drawer) {
+        case 'dealer':
+            if (sum === 21) {
+                console.log('Dealer Blackjack!');
+            } else if (sum > 21) {
+                console.log('Dealer bust');
+            } else if (sum < 17) {
+                DealerHand.unshift(DealerDeck.deck.shift());
+                DealerHand[DealerHand.length-1] += cardEval(DealerHand[0].value);
+                newCard('#dealersHand',DealerHand[0]);
+                bustCheck('dealer',DealerHand[DealerHand.length-1]);
+            } else {
+                console.log(sum);
+            }
+            break;
+        case 'player':
+            if (sum === 21) {
+                console.log('Blackjack!');
+            } else if (sum > 21) {
+                $('#draw-button').prop('disabled', true);
+                $('#draw-button').html('Bust!');
+                console.log('Bust!');
+            } else {
+                console.log(sum);
+            }
+            break;
+    }
+};
+
+/* function handStopper(sum) {
     if (sum > 21) {
         bust();
     }
@@ -102,9 +184,8 @@ function handStopper(sum) {
     else {
         return;
     }
-}
+} */
 
-function bust() {
+/* function bust() {
     $('#stand-button').addClass('.disabled');
-    
-}
+} */
